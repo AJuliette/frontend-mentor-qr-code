e5b64c156ea2dace8b06ecf1c9369dbc1163c7f6
Juliette Audema <juliette.audema@gmail.com>
2024-08-09 10:53:09 +0200

    Improve design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: ['./index.html'],
   theme: {
     extend: {
-      fontSize: {
-        paragraph: "15px",
-      },
     },
     fontFamily: {
       outfit: ["Outfit", "sans-serif"],
